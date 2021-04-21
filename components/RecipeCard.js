6bc5bc0fbfa2603be6d1e5099cee69e3ec280c11
Umut Akin <umutakin@fastmail.com>
2021-04-21 16:40:38 +0300

    nextjs contentful 10 - incremental static regeneration

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -17,7 +17,7 @@ export default function RecipeCard({ recipe }) {
                 <div className="content">
                     <div className="info">
                         <h4>{title}</h4>
-                        <p>Takes approx. {cookingTime} mins to make</p>
+                        <p>Takes approx. {cookingTime } mins to make</p>
                     </div>
                     <div className="actions">
                         <Link href={"/recipes/" + slug}>
