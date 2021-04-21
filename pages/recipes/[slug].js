9b20c18f27728a2492c5ef7dd1fcfb8fe8412bf5
Umut Akin <umutakin@fastmail.com>
2021-04-21 17:00:23 +0300

    nextjs contentful 11 - fallback pages

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     };
 };
 
@@ -32,11 +33,19 @@ export async function getStaticProps({ params }) {
 
     return {
         props: { recipe: items[0] },
-        revalidate: 1
+        revalidate: 1,
     };
 }
 
 export default function RecipeDetails({ recipe }) {
+    if (!recipe) {
+        return (
+            <div>
+                <Skeleton />
+            </div>
+        );
+    }
+
     const {
         featuredImage,
         title,
